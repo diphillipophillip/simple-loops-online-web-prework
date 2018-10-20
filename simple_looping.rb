7b185c0e79d08add18d02a382aeb1be75b784a6d
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-20 20:37:13 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for counter in number_of_times 
+  for counter in 1..number_of_times do
   puts phrase
 end
 end
